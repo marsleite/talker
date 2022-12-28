cab1f727bf26afc3c2cff160bf4acdb189725a40
Marcelo <marcelo.leite@mercadolivre.com>
2022-12-28 18:41:32 -0300

    [feat] create talk entity and relation Entity

--- src/main/java/com/example/talker/domain/entity/Talk.java
+++ src/main/java/com/example/talker/domain/entity/Talk.java
@@ -2,25 +2,18 @@ package com.example.talker.domain.entity;
 
 import lombok.Data;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
-public class User {
-
+public class Talk {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
-  private String name;
-  private Integer age;
-
-  @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "talk_id")
-  private Talk talk;
+  private LocalDateTime watchedAt;
+  private Integer rate;
 }
