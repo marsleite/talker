c3057bec74a293c3433b9d5f79ee3722fe741693
Marcelo <marcelo.leite@mercadolivre.com>
2022-12-28 18:15:58 -0300

    [feat] initial config

---
+++ src/main/java/com/example/talker/TalkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.talker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TalkerApplication.class, args);
+  }
+
+}
